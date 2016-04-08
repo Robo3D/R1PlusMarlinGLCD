32f479cafa83f57c4161681fcc7cfa9c41daa32a
Brictone <briang@bg3d.com>
2016-04-07 17:36:50 -0700

    Update Configuration.h
    #define ENCODER_STEPS_PER_MENU_ITEM 4 seems to work pretty good.  Not sure about high low sensitivity yet.

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -462,7 +462,7 @@ const bool Z_MAX_ENDSTOP_INVERTING = false; // set to true to invert the logic o
 #define DEFAULT_ZJERK                 0.4     // (mm/sec)
 #define DEFAULT_EJERK                 5.0    // (mm/sec)
 
-#define DEFAULT_ENCODER_STEPS_PER_MENU_ITEM 5
+#define DEFAULT_ENCODER_STEPS_PER_MENU_ITEM 4
 #define DEFAULT_ENCODER_PULSES_PER_STEP 1
 
 
@@ -505,7 +505,7 @@ const bool Z_MAX_ENDSTOP_INVERTING = false; // set to true to invert the logic o
 //#define SDSUPPORT // Enable SD Card Support in Hardware Console
 #define SDSLOW // Use slower SD transfer mode (not normally needed - uncomment if you're getting volume init error)
 #define ENCODER_PULSES_PER_STEP 1 // Increase if you have a high resolution encoder
-#define ENCODER_STEPS_PER_MENU_ITEM 5 // Set according to ENCODER_PULSES_PER_STEP or your liking
+#define ENCODER_STEPS_PER_MENU_ITEM 4 // Set according to ENCODER_PULSES_PER_STEP or your liking
 //#define ULTIMAKERCONTROLLER //as available from the Ultimaker online store.
 //#define ULTIPANEL  //the UltiPanel as on Thingiverse
 //#define LCD_FEEDBACK_FREQUENCY_HZ 1000	// this is the tone frequency the buzzer plays when on UI feedback. ie Screen Click
